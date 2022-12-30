e18db6a899ff4bd0faffa5fc79143ad2465a44c3
juhe1 <38502950+juhe1@users.noreply.github.com>
2022-12-30 03:51:33 +0200

    login is done

--- extension/js/tanki_flash_interface_main.js
+++ extension/js/tanki_flash_interface_main.js
@@ -2,10 +2,26 @@ currentLayout = null
 
 function loadStyle(styleFilePath) {
 	const path = chrome.extension.getURL(styleFilePath);
-	
+
 	// add new style to head element
 	const headElement = document.getElementsByTagName('head')[0];
 	headElement.innerHTML += '<link id="customCss" rel="stylesheet" href="' + path + '">';
+}
+
+async function createIframe(htmlFilePath, id) {
+	const path = chrome.extension.getURL(htmlFilePath);
+
+	// read html file
+	const response = await fetch(path);
+	const htmlCode = await response.text();
+
+	const element = document.createElement("iframe");
+	element.id = id;
+	document.body.insertBefore(element, document.body.firstChild);
+
+	iframe = document.getElementById(id);
+	iframe.srcdoc = htmlCode;
+	return iframe
 }
 
 async function loadHtmlToBody(htmlFilePath) {
@@ -14,45 +30,89 @@ async function loadHtmlToBody(htmlFilePath) {
 	// read html file
 	const response = await fetch(path);
 	const htmlCode = await response.text();
-	console.log(htmlCode);
-	
+
 	// add html to body
 	const bodyElement = document.getElementsByTagName('body')[0];
 	bodyElement.innerHTML = htmlCode + bodyElement.innerHTML;
 	return bodyElement
 }
 
+function writeToInputBox(inputBoxId, text) {
+	const inputBox = document.getElementById(inputBoxId);
+	inputBox.value = "";
+	inputBox.focus({preventScroll: true});
+	document.execCommand('insertText', false, text);
+}
+
 class LoadinScreenLayout {
 	constructor() {
 		this.layoutName = "LoadinScreenLayout";
 	}
-	
+
 	load() {
+		loadStyle("css/main.css");
 		loadStyle("css/background.css");
 	}
-	
+
 	unload() {
-		
+
 	}
 }
 
 class LoginLayout {
 	constructor() {
 		this.layoutName = "LoginLayout";
+		this.iframe = null;
+		this.iframeDocument = null;
 	}
-	
+
 	load() {
-		loadStyle("css/login/bottom_bar.css");
-		loadStyle("css/login/check_mark.css");
-		loadStyle("css/login/login.css");
-		loadStyle("css/login/play_button.css");
-		
-		var newElement = loadHtmlToBody("html/login.html");
-		newElement.getElementById("login_header").src = chrome.extension.getURL("images/tank_window/login_header.png");
+		this.createLoginIframe()
 	}
-	
+
+	async createLoginIframe() {
+		this.iframe = await createIframe("html/login.html", "login");
+
+		this.iframe.addEventListener('load', () => {
+			this.iframeDocument = this.iframe.contentWindow.document;
+			this.iframeDocument.getElementById("play_button").onmousedown = () => {this.playClicked()};
+    });
+	}
+
+	playClicked() {
+		const userName = this.iframeDocument.getElementById("username_input").value;
+		const password = this.iframeDocument.getElementById("password_input").value;
+		const rememberMe = this.iframeDocument.getElementById("check_box").checked;
+
+		this.tryLogin(userName, password, rememberMe);
+	}
+
+	setRememberMeCheckBox(state) {
+		const onElement = document.getElementsByClassName("sc-bxivhb bCVAbE");
+		const offElement = document.getElementsByClassName("sc-bxivhb knLUAV");
+
+		if(offElement.length == 0 && state == false) {
+			onElement[0].click();
+			return;
+		}
+		if (onElement.length == 0 && state == true) {
+			offElement[0].click()
+			return;
+		}
+
+	}
+
+	tryLogin(userName, password, rememberMe) {
+		writeToInputBox("username", userName);
+		writeToInputBox("password", password);
+		this.setRememberMeCheckBox(rememberMe)
+
+		// emulate play button click
+		const originalPlayButton = document.getElementsByClassName("sc-bwzfXH jplTTR")[0];
+		originalPlayButton.click()
+	}
+
 	unload() {
-		
 	}
 }
 
@@ -62,9 +122,9 @@ function checkForLoginLayout(_currentLayout) {
 			return;
 		}
 	}
-	
+
 	const userNameElement = document.getElementById("username");
-	
+
 	if (userNameElement != null) {
 		return new LoginLayout();
 	}
@@ -76,9 +136,9 @@ function checkForLoadingLayout(_currentLayout) {
 			return null;
 		}
 	}
-	
+
 	const progressElement = document.getElementById("loading-text");
-	
+
 	if (progressElement != null) {
 		return new LoadinScreenLayout();
 	}
@@ -93,13 +153,13 @@ const checkFunctions = [
 function checkLayout() {
 	for (const checkFunction of checkFunctions) {
 		layout = checkFunction(currentLayout);
-		
+
 		if (layout != null) {
-			
+
 			if (currentLayout != null) {
 				currentLayout.unload();
 			}
-			
+
 			currentLayout = layout;
 			console.log("current layout: " + currentLayout.layoutName)
 			layout.load();
@@ -110,14 +170,14 @@ function checkLayout() {
 function disableCss() {
 	// style elements with these ids will not be disabled
 	cssWhiteList = ["customCss"]
-	
+
 	for (const elementWithStyleAtribute of document.querySelectorAll('[style]')) {
 		if (cssWhiteList.includes(elementWithStyleAtribute)) {
 			continue;
 		}
 		elementWithStyleAtribute.removeAttribute("style");
 	}
-	
+
 	for ( i=0; i<document.styleSheets.length; i++) {
 		const styleId = document.styleSheets.item(i).ownerNode.id;
 		if (cssWhiteList.includes(styleId)) {
@@ -133,8 +193,8 @@ function initLayoutChangeDetector() {
 		disableCss(); // TODO: this is kinda laggy so change it to disable css only from new elements
 		checkLayout();
 	}
-	
-	// init MutationObserver 
+
+	// init MutationObserver
 	var mutationObserver = new MutationObserver(mutationCallback);
 	const config = {attributes: true, childList: true, subtree: true};
 	mutationObserver.observe(document, config);
